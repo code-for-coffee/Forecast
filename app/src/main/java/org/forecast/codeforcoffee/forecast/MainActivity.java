2698ed279255cda26c7b3e951df8bedd573974f3
James Traver <jtraverjr@gmail.com>
2016-04-03 22:55:29 -0500

    oy eh

--- app/src/main/java/org/forecast/codeforcoffee/forecast/MainActivity.java
+++ app/src/main/java/org/forecast/codeforcoffee/forecast/MainActivity.java
@@ -1,10 +1,14 @@
 package org.forecast.codeforcoffee.forecast;
 
+import android.app.ActionBar;
 import android.app.Activity;
+import android.app.FragmentTransaction;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.FrameLayout;
 import android.widget.ListView;
@@ -21,19 +25,23 @@ import java.util.List;
 
 public class MainActivity extends Activity {
 
-
+    private ForecastFragement forecastFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        FrameLayout frame = new FrameLayout(this);
+        frame.setId(R.id.primary_framelayout);
+        setContentView(frame, new ActionBar.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT));
 
-        // These two need to be declared outside the try/catch
-        // so that they can be closed in the finally block.
 
-
-
-
+        if (savedInstanceState == null) {
+            forecastFragment = new ForecastFragement();
+            FragmentTransaction transaction = getFragmentManager().beginTransaction();
+            transaction.add(R.id.primary_framelayout, forecastFragment).commit();
+        }
 
     }
 }
